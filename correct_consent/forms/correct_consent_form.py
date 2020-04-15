8f1aa0915a6931742dafeab2783dc4b82d178b67
tumiemosweu <imosweuk@gmail.com>
2020-04-15 08:35:13 +0200

    add tests

--- correct_consent/forms/correct_consent_form.py
+++ correct_consent/forms/correct_consent_form.py
@@ -1,8 +1,72 @@
 from django import forms
+from edc_form_validators import FormValidator
 from ..models import CorrectConsent
 
 
-class CorrectConsentForm(forms.ModelForm):
+class CorrectConsentForm(FormValidator, forms.ModelForm):
+
+    def clean(self):
+#         cleaned_data = super().clean()
+
+        self.subject_consent = self.cleaned_data.get('subject_consent')
+
+        self.validate_identity(subject_consent=self.subject_consent)
+        self.validate_initials()
+
+        fields = ['identity', 'first_name', 'last_name', 'dob', 'gender',
+                  'guardian_nam', 'may_store_samples', 'is_literate',
+                  'witness_name']
+
+        for f in fields:
+            self.required_if_not_none(
+                field='new' + f,
+                field_required='old' + f)
+
+    def validate_identity(self, subject_consent=None):
+        old_identity = self.cleaned_data.get('old_identity')
+        if (old_identity
+                and subject_consent.identity != old_identity):
+
+            raise forms.ValidationError(
+                {'old_identity': f'The old_identity does not match the '
+                 f'identity from the subject\'s consent. Got {old_identity}, '
+                 f'expected {subject_consent.identity}.'})
+
+    def validate_initials(self):
+
+        subject_consent = self.cleaned_data.get('subject_consent')
+
+        first_name = self.cleaned_data.get('new_first_name') or subject_consent.first_name
+
+        last_name = self.cleaned_data.get('new_last_name') or subject_consent.last_name
+
+        new_initials = self.cleaned_data.get('new_initials')
+
+        old_initials = self.cleaned_data.get('old_initials')
+        if old_initials:
+
+            if subject_consent.initials != old_initials:
+
+                raise forms.ValidationError(
+                    {'old_initials': f'The old_intials does not match the '
+                     f'initials from the subject\'s consent. Got {old_initials}, '
+                     f'expected {subject_consent.initials}.'})
+
+            if (new_initials and not (new_initials.startswith(first_name[0])
+                                      and new_initials.endswith(last_name[0]))):
+                raise forms.ValidationError(
+                    {'new_initials': 'New initials do not match first and '
+                     'last name. Expected initials that start with '
+                     f'{first_name[0]} and end with {last_name[0]}, '
+                     f'Got {self.new_initials}'})
+
+#     def validate_new_name(self):
+
+#     def validate_old_field(self, consent=None, old_field=None):
+#
+#         if consent and old_field:
+#             consent_field = old_field[4:]
+#             if consent
 
     class Meta:
         model = CorrectConsent
--- correct_consent/tests/tests.py
+++ correct_consent/tests/tests.py
@@ -1,4 +1,5 @@
-from django.test import TestCase
+from django.core.exceptions import ValidationError
+from django.test import TestCase, tag
 from edc_base.utils import get_utcnow
 from model_mommy import mommy
 
@@ -6,6 +7,7 @@ from ..forms import CorrectConsentForm
 from ..models import SubjectConsent
 
 
+@tag('cc')
 class TestCorrectConsent(TestCase):
 
     def setUp(self):
@@ -16,22 +18,61 @@ class TestCorrectConsent(TestCase):
             'old_identity': self.subject_consent.identity,
             'new_identity': '44425678',
             'report_datetime': get_utcnow(),
-            'subject_consent': self.subject_consent
+            'subject_consent': self.subject_consent,
+            'new_first_name': None,
+            'old_first_name': None,
+            'new_last_name': None,
+            'old_last_name': None
         }
 
-#         correct_consent = CorrectConsent.objects.create(
-#             old_identity=self.subject_consent,
-#             new_identity='44425678')
-
     def test_form_is_ok(self):
-        correct_consent_form = CorrectConsentForm(data=self.options)
+        correct_consent_form = CorrectConsentForm(cleaned_data=self.options)
         self.assertIsInstance(self.subject_consent, SubjectConsent)
-        self.assertTrue(correct_consent_form.is_valid())
-        self.assertTrue(correct_consent_form.save())
+        try:
+            correct_consent_form.validate()
+        except ValidationError as e:
+            self.fail(f'ValidationError unexpectedly raised. Got{e}')
 
-    def test_identity_match(self):
+    def test_identity_match_invalid(self):
         self.options['old_identity'] = '44424444'
-        correct_consent_form = CorrectConsentForm(data=self.options)
-        self.assertIsInstance(self.subject_consent, SubjectConsent)
-        self.assertTrue(correct_consent_form.is_valid())
-        self.assertTrue(correct_consent_form.save())
+        correct_consent_form = CorrectConsentForm(cleaned_data=self.options)
+        self.assertRaises(ValidationError, correct_consent_form.validate)
+        self.assertIn('old_identity', correct_consent_form._errors)
+
+    def test_old_initials_match_invalid(self):
+        self.options['old_initials'] = 'TT'
+        correct_consent_form = CorrectConsentForm(cleaned_data=self.options)
+        self.assertRaises(ValidationError, correct_consent_form.validate)
+        self.assertIn('old_initials', correct_consent_form._errors)
+
+    def test_initials_match_valid(self):
+        self.options['old_initials'] = self.subject_consent.initials
+        correct_consent_form = CorrectConsentForm(cleaned_data=self.options)
+        try:
+            correct_consent_form.validate()
+        except ValidationError as e:
+            self.fail(f'ValidationError unexpectedly raised. Got{e}')
+
+    def test_no_old_identity_new_invalid(self):
+        self.options['old_identity'] = None
+        correct_consent_form = CorrectConsentForm(cleaned_data=self.options)
+        self.assertRaises(ValidationError, correct_consent_form.validate)
+        self.assertIn('old_identity', correct_consent_form._errors)
+
+    def test_no_old_first_name_new_invalid(self):
+        self.options['old_first_name'] = None
+        self.options['new_first_name'] = self.subject_consent.first_name
+        correct_consent_form = CorrectConsentForm(cleaned_data=self.options)
+        self.assertRaises(ValidationError, correct_consent_form.validate)
+        self.assertIn('old_first_name', correct_consent_form._errors)
+
+#     def test_old_initials_match_invalid(self):
+#         self.options['old_initials'] = self.subject_consent.initials
+#         self.options['old_initials'] = 'TT'
+#         correct_consent_form = CorrectConsentForm(data=self.options)
+#         self.assertFalse(correct_consent_form.is_valid())
+#         self.assertIn('old_initials', correct_consent_form.errors)
+
+#         self.assertIsInstance(self.subject_consent, SubjectConsent)
+#         self.assertTrue(correct_consent_form.is_valid())
+#         self.assertTrue(correct_consent_form.save()) form.errors
